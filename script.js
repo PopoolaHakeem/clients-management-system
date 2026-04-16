54449681e9bcd007c21fc8444882dd7c0b33ac3a
PopoolaHakeem <hakeempopoola04@gmail.com>
2026-04-16 11:09:21 +0100

    first commit

--- script.js
+++ script.js
@@ -3,6 +3,16 @@ const form = document.getElementById("loginForm");
 form.addEventListener("submit", function(e) {
   e.preventDefault();
 
+  const mail = document.getElementById("email").value;
+
+
+  if (mail === "") {
+    alert("Please enter your email.");
+    return;
+  } else {
+    alert("Login successful!");
+  }
+
   const email = document.getElementById("email").value;
 
   localStorage.setItem("clientEmail", email);
