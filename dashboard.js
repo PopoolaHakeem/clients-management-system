44d3047492dfeab9bd5ca6c03fc40fa9e222e3f9
PopoolaHakeem <hakeempopoola04@gmail.com>
2026-04-30 18:30:48 +0100

    mobile menu

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,3 @@
-const container = document.getElementById("projectList");
-
-const email = localStorage.getItem("clientEmail");
-const projects = JSON.parse(localStorage.getItem("projects")) || [];
-
-const userProjects = projects.filter(p => p.email === email);
 
 const menuBtn = document.getElementById("menuBtn");
 const mobileMenu = document.getElementById("mobileMenu");
@@ -11,6 +5,20 @@ const mobileMenu = document.getElementById("mobileMenu");
 menuBtn.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
+
+const container = document.getElementById("projectList");
+
+const email = localStorage.getItem("clientEmail");
+const projects = JSON.parse(localStorage.getItem("projects")) || [];
+
+const userProjects = projects.filter(p => p.email === email);
+
+// const menuBtn = document.getElementById("menuBtn");
+// const mobileMenu = document.getElementById("mobileMenu");
+
+// menuBtn.addEventListener("click", () => {
+//   mobileMenu.classList.toggle("hidden");
+// });
 
 let total = userProjects.length;
 let ongoing = 0;
